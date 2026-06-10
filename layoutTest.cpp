d7c9270ddb1124e9405278ecff092715eb124b25
bryanwongcookie <bryankjwong@gmail.com>
2026-06-10 14:42:21 +0800

    Implement dynamic board size selection in layoutTest
    Added dynamic board size selection for testing.

--- layoutTest.cpp
+++ layoutTest.cpp
@@ -20,10 +20,21 @@ void debugDisplayBoard(char board[MAX_ROWS][MAX_COLS], int boardSize);
 
 int main()
 {
-    // TEMPORARY SETUP: simulate a chosen board size (exp: 6x6, 8x8 or 10x10)
+   // TEMPORARY SETUP: simulate a chosen board size (exp: 6x6, 8x8 or 10x10)
+    // TEMPORARY DYNAMIC BOARD SELECTION TESTING
     int  testBoardSize = 6;
     char testBoard[MAX_ROWS][MAX_COLS];
 
+    cout << "Select a Board Size: ";
+    cin  >> testBoardSize;
+    while(testBoardSize > 10 || testBoardSize < 6)
+    {
+        cout << "ERROR: Board Size cannot be less than 6X6 or more than 10X10. Please try again!" << endl;
+        cout << "Select a Board Size: ";
+        cin  >> testBoardSize;
+    }
+
+    cout << "Board Size Selected: " << testBoardSize << "X" << testBoardSize << endl;
     cout << "---Testing Piece Placement Logic---" << endl;
     initializeNewBoard(testBoard, testBoardSize);
     debugDisplayBoard(testBoard, testBoardSize);
