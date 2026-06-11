86e295e66a457db772d83e0b0bdaad46871bb846
trisadel <trishaadelina07@gmail.com>
2026-06-11 12:39:53 +0800

    Update layoutTest.cpp
    Transferred my code (save/load) to my own branch

--- layoutTest.cpp
+++ layoutTest.cpp
@@ -19,50 +19,8 @@ bool convertInputToCoordinates(string input, int &row, int &col, int boardSize);
 void initializeNewBoard(char board[MAX_ROWS][MAX_COLS], int boardSize);
 void debugDisplayBoard(char board[MAX_ROWS][MAX_COLS], int boardSize);
 
-
-bool checkSaveFile()
-bool loadGame(char board[MAX_ROWS][MAX_COLS], int &boardSize, char &currentTurn );
-void startMenu (char board[MAX_ROWS][MAX_COLS], int &boardSize, char &currentTurn);
-void mainGameLoop (char board[MAX_ROWS][MAX_COLS], int boardSize, char startingTurn);
-
 int main()
 {
- /*
----------------------------------------------------------------------------------
-DEVELOPER: Trisha Adelina Binti Shuhaini
-TASK     : Start Menu and Save/Load Logic
----------------------------------------------------------------------------------
-*/
-
-    string menuChoice;
-    int activeBoardSize = 6; // default size for game state
-    char activeBoard[MAX_ROWS][MAX_COLS];
-
-    cout << "Type 'NEW GAME' or 'CONTINUE': ";
-    getline(cin, menuChoice);
-
-    // convert to uppercase
-    for (int i = 0; i < menuChoice.length(); i++) {
-        menuChoice[i] = toupper(menuChoice[i]);
-    }
-
-    if (menuChoice = "CONTINUE"){
-        if (checkSaveFile()){
-            cout >> "File save found. Loading state" << endl;
-            loadGame(activeBoard, activeBoardSize);
-        } else {
-        cout >> "No save file. Creating new" << endl;
-        initializeNewBoard(activeBoard, activeBoardSize);
-        }
-    } else {
-    // default new game
-    initializeNewBoard(activeBoard, activeBoardSize);
-    }
-
-
-
-
-
 
     // TEMPORARY SETUP: simulate a chosen board size (exp: 6x6, 8x8 or 10x10)
     int  testBoardSize = 6;
@@ -211,39 +169,4 @@ void debugDisplayBoard(char board[MAX_ROWS][MAX_COLS], int boardSize)
         cout << endl;
     }
 }
-/*
-------------------------------------------------------------------------------
-DEVELOPER: Trisha Adelina Binti Shuhaini
-TASK     : save/load to  see if save file exist
-------------------------------------------------------------------------------
-*/
 
-bool checkSaveFile()
-{
-    ifstream file ("savegame.txt");
-    return file.good();
-}
-
-/*
----------------------------------------------------------------------------------
-DEVELOPER: Trisha Adelina Binti Shuhaini
-TASK     : extract data from save/load
----------------------------------------------------------------------------------
-*/
-void loadGame(char board[MAX_ROWS][MAX_COLS], int &boardSize)
-{
-    ifstream file("savegame.txt");
-    if (!file.is_open()) return;
-
-    file >> boardSize;
-    for (int r = 0; r < boardSize; r++) {
-        for (int c = 0; c < boardSize; c++){
-            file >> board [r][c]
-            if (board[r][c] == '_'){
-                 board[r][c] = ' ';
-        }
-      }
-
-    }
-    file.close();
-}
